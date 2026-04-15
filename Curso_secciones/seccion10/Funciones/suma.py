9ccb84c3156d11242caf852a0510130ffd40d1e0
Alex Andres Yoza <alextumbaco28@gmail.com>
2026-04-15 15:56:49 -0500

    curso_

---
+++ Curso_secciones/seccion10/Funciones/suma.py
@@ -0,0 +1,9 @@
+# import modulo_funcion_sumar
+
+from Curso_secciones.seccion10.Funciones.modulo_funcion_sumar import sumar
+
+print("Suma funcion")
+
+# Llamar a la funcion
+resultado_funcion = sumar(8, 5)
+print(f"Resultado de la suma: {resultado_funcion}")
--- seccion10/Funciones/suma.py
+++
@@ -1,8 +0,0 @@
-#import modulo_funcion_sumar
-
-from modulo_funcion_sumar import sumar
-print("Suma funcion")
-
-#Llamar a la funcion
-resultado_funcion = sumar(8, 5)
-print(f"Resultado de la suma: {resultado_funcion}")
